72e03aa8ede558562e84b7acebd4dc513735cd89
fromitive <fromitive@gmail.com>
2025-03-31 12:22:25 +0900

    test: 패스키 중복 등록 테스트 추가 refactor: 테스트 코드 리팩터링

--- src/test/java/com/sign/application/usecase/PasskeyRegistrationUseCaseTest.java
+++ src/test/java/com/sign/application/usecase/PasskeyRegistrationUseCaseTest.java
@@ -3,14 +3,12 @@ package com.sign.application.usecase;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.sign.application.repository.HandleGenerator;
 import com.sign.application.repository.PasskeyRepository;
 import com.sign.infrastructure.repository.HandleGeneratorImpl;
-import com.yubico.webauthn.RegisteredCredential;
 import com.yubico.webauthn.RelyingParty;
 import com.yubico.webauthn.StartRegistrationOptions;
 import com.yubico.webauthn.data.AuthenticatorAttestationResponse;
@@ -26,11 +24,11 @@ import de.adesso.softauthn.CredentialsContainer;
 import de.adesso.softauthn.Origin;
 import de.adesso.softauthn.authenticator.WebAuthnAuthenticator;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 class PasskeyRegistrationUseCaseTest {
@@ -63,7 +61,7 @@ class PasskeyRegistrationUseCaseTest {
     }
 
     @Nested
-    @DisplayName("Registration Start 시")
+    @DisplayName("Registration Start 테스트")
     class WhenRegistrationStart {
         private final RelyingParty relyingParty = Mockito.mock(RelyingParty.class);
         private final HandleGenerator handleGenerator = Mockito.mock(HandleGenerator.class);
@@ -99,63 +97,67 @@ class PasskeyRegistrationUseCaseTest {
     @Nested
     @DisplayName("Registration Finish 테스트")
     class Test2 {
-        private final RelyingParty relyingParty = RelyingPartyFixture.create(passkeyRepository);
         private final HandleGenerator handleGenerator = new HandleGeneratorImpl(32);
-        private final WebAuthnAuthenticator authenticator = Authenticators.yubikey5Nfc().build();
+        private final PasskeyRepository passkeyRepository = new InMemoryPasskeyRepository();
+        private final RelyingParty relyingParty = RelyingPartyFixture.create(passkeyRepository);
         private final PasskeyRegistrationUseCase passkeyRegistrationUseCase = new PasskeyRegistrationUseCase(
                 passkeyRepository,
                 relyingParty,
                 handleGenerator
         );
+        private final WebAuthnAuthenticator authenticator = Authenticators.yubikey5Nfc().build();
+        private final Origin origin = new Origin("https", "sign.co.kr", -1, null);
+        private final CredentialsContainer container = new CredentialsContainer(origin, List.of(authenticator));
+        private PublicKeyCredential<AuthenticatorAttestationResponse, ClientRegistrationExtensionOutputs> credential;
+        private PublicKeyCredentialCreationOptions options;
 
-        @Nested
-        @DisplayName("올바른 Origin을 설정하고 요청하면")
-        class WhenCorrectOrigin {
-            Origin origin = new Origin("https", "sign.co.kr", -1, null);
-            CredentialsContainer container = new CredentialsContainer(origin, List.of(authenticator));
-            PublicKeyCredential<AuthenticatorAttestationResponse, ClientRegistrationExtensionOutputs> credential;
-            PublicKeyCredentialCreationOptions options;
-
-            @BeforeEach
-            void setUp() {
-                options = passkeyRegistrationUseCase.start(email);
-                credential = container.create(options);
-            }
-
-            @Test
-            @DisplayName("예외가 발생하지 않는다.")
-            void test1() {
-                assertThatCode(() -> passkeyRegistrationUseCase.finish(options, credential, email))
-                        .doesNotThrowAnyException();
-            }
-
-            @Test
-            @DisplayName("해당 이메일의 패스키가 저장된다.")
-            void test2() {
-                passkeyRegistrationUseCase.finish(options, credential, email);
-                verify(passkeyRepository).save(ArgumentMatchers.eq(email), any(RegisteredCredential.class));
-            }
+        @BeforeEach
+        void setUp() {
+            options = passkeyRegistrationUseCase.start(email);
+            credential = container.create(options);
         }
 
-        @Nested
-        @DisplayName("올바른 Origin을 설정하지 않으면")
-        class WhenInCorrectOrigin {
+        @Test
+        @DisplayName("패스키 정상 등록 시 예외가 발생하지 않는다.")
+        void test1() {
+            assertThatCode(() -> passkeyRegistrationUseCase.finish(options, credential, email))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("해당 이메일의 패스키가 저장된다.")
+        void test2() {
+            passkeyRegistrationUseCase.finish(options, credential, email);
+            Optional<ByteArray> credential = passkeyRepository.getUserHandleForUsername(email);
+            boolean actual = credential.isPresent();
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        @DisplayName("패스키를 중복으로 저장할 경우 예외가 발생한다.")
+        void test3() {
+            passkeyRegistrationUseCase.finish(options, credential, email);
+            assertThatThrownBy(() -> passkeyRegistrationUseCase.finish(options, credential, email));
+        }
+
+        @Test
+        @DisplayName("다른 기기를 등록할 경우 예외가 발생하지 않는다.")
+        void test4() {
+            PublicKeyCredentialCreationOptions otherOption = passkeyRegistrationUseCase.start(email);
+            PublicKeyCredential<AuthenticatorAttestationResponse,
+                    ClientRegistrationExtensionOutputs> otherCredential = container.create(options);
+
+            assertThatCode(() -> passkeyRegistrationUseCase.finish(otherOption, otherCredential, email));
+        }
+
+        @Test
+        @DisplayName("다른 Origin으로 등록할 경우 예외가 발생한다.")
+        void test5() {
             Origin origin = new Origin("https", "example.com", -1, null);
             CredentialsContainer container = new CredentialsContainer(origin, List.of(authenticator));
-            PublicKeyCredential<AuthenticatorAttestationResponse, ClientRegistrationExtensionOutputs> credential;
-            PublicKeyCredentialCreationOptions options;
-
-            @BeforeEach
-            void setUp() {
-                options = passkeyRegistrationUseCase.start(email);
-                credential = container.create(options);
-            }
-
-            @Test
-            @DisplayName("예외가 발생한다.")
-            void test1() {
-                assertThatThrownBy(() -> passkeyRegistrationUseCase.finish(options, credential, email));
-            }
+            options = passkeyRegistrationUseCase.start(email);
+            credential = container.create(options);
+            assertThatThrownBy(() -> passkeyRegistrationUseCase.finish(options, credential, email));
         }
     }
 }
