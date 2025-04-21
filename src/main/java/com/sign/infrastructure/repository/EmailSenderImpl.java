6804f92a7f63142aa2edff89602c001ec5498df9
robinjoon <robin980108@naver.com>
2025-04-21 22:25:39 +0900

    refactor: 이메일 전송 비동기 처리 Spring @Async 어노테이션을 이용하도록 변경

--- src/main/java/com/sign/infrastructure/repository/EmailSenderImpl.java
+++ src/main/java/com/sign/infrastructure/repository/EmailSenderImpl.java
@@ -3,33 +3,23 @@ package com.sign.infrastructure.repository;
 import com.sign.application.repository.EmailSender;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import java.util.concurrent.Executor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 @Slf4j
+@RequiredArgsConstructor
 @Repository
 public class EmailSenderImpl implements EmailSender {
 
     private final JavaMailSender javaMailSender;
 
-    private final Executor executor;
-
-    // Qualifier 를 사용하기 위해서는 롬복을 이용할 수 없습니다.
-    public EmailSenderImpl(JavaMailSender javaMailSender, @Qualifier("emailTaskExecutor") Executor executor) {
-        this.javaMailSender = javaMailSender;
-        this.executor = executor;
-    }
-
     @Override
+    @Async("emailTaskExecutor")
     public void send(String from, String to, String subject, String body) {
-        executor.execute(() -> sendMailWithJavaMailSender(from, to, subject, body));
-    }
-
-    private void sendMailWithJavaMailSender(String from, String to, String subject, String body) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
--- src/main/java/com/sign/infrastructure/repository/config/AsyncConfig.java
+++ src/main/java/com/sign/infrastructure/repository/config/AsyncConfig.java
@@ -4,8 +4,10 @@ import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.VirtualThreadTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
 
     @Bean(name = "emailTaskExecutor")
