ee1d3596f8a03442d328bfeaeba99d053329d903
fromitive <fromitive@gmail.com>
2025-05-06 15:37:58 +0900

    feat: 임시 가입 페이지 기능 구현

---
+++ src/main/java/com/sign/controller/StaticPageController.java
@@ -0,0 +1,13 @@
+package com.sign.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StaticPageController {
+
+    @GetMapping("registration")
+    String register() {
+        return "registration";
+    }
+}
