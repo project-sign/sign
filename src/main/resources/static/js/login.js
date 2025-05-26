e0871eabfaed2a88fe89c63a3a18f4fb16bd2a05
fromitive <fromitive@gmail.com>
2025-05-26 13:56:46 +0900

    feat: login challenge request 기능 구현

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -6,6 +6,21 @@ console.log(appId);
 
 // passkey login
 // 1. assertion challenge request (get /assertion)
+(() => {
+    fetch("/passkey/assertion")
+        .then(response => {
+            if (!response.ok) throw new Error("서버 오류");
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            // data.data 로 "2. passkey 추출" 구현
+        })
+        .catch(err => {
+            console.error("API 실패:", err);
+        });
+})();
+
 // 2. passkey 추출
 // 3. assertion finish request(post /assertion)
 // 4. TODO: post /assertion이 access_token을 반환하도록 변경
