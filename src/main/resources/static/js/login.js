6f057cbfdb64ea77dc2a661600a8344ebf89a332
fromitive <fromitive@gmail.com>
2025-05-26 14:54:22 +0900

    feat: 패스키 로그인 요청 기능 구현

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -22,16 +22,13 @@ console.log(appId);
 })();
 
 // 2. passkey 추출
-
 function toBase64Url(base64) {
     return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 }
 
 function base64UrlToBase64(base64Url) {
-    // URL-safe 형식을 표준 base64로 변환
     let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
 
-    // 패딩 추가 (길이가 4의 배수가 아닐 경우 '=' 추가)
     while (base64.length % 4) {
         base64 += '=';
     }
@@ -44,18 +41,44 @@ async function loginPasskey(challenge) {
             challenge: Uint8Array.from(atob(base64UrlToBase64(challenge)), c => c.charCodeAt(0)),
             userVerification: "preferred",
         };
-        // 브라우저 passkey 인증 기능 호출
+
         const credential = await navigator.credentials.get({
               publicKey: publicKey
         });
 
-        console.log(credential);
+        const assertionResponse = {
+            id: credential.id,
+            rawId: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.rawId)))),
+            type: credential.type,
+            response: {
+                authenticatorData: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.authenticatorData)))),
+                clientDataJSON: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.clientDataJSON)))),
+                signature: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.signature)))),
+                userHandle: credential.response.userHandle ? toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.userHandle)))) : null
+            },
+            clientExtensionResults: credential.getClientExtensionResults()
+        };
+
+        const passkeyResponse = await fetch(`/passkey/assertion`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(assertionResponse)
+        });
+
+        if (passkeyResponse.ok) {
+            console.log(passkeyResponse)
+        } else {
+            alert("Login failed.");
+        }
     } catch (error) {
         console.error("패스키 로그인이 실패했습니다.", error);
     }
 }
-// 3. assertion finish request(post /assertion)
+
 // 4. TODO: post /assertion이 access_token을 반환하도록 변경
+
 
 
 
