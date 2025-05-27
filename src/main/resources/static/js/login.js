c260f0a1d03f9877660526ef47b9cd49936e6614
fromitive <fromitive@gmail.com>
2025-05-27 13:24:55 +0900

    refactor: 자동 로그인이 아닌 버튼 클릭 시 로그인 하도록 변경

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -4,22 +4,26 @@ const params = new URLSearchParams(window.location.search);
 const appId = params.get('appId');
 console.log(appId);
 
+const passKeyLoginButton = document.querySelector("#login-button");
+const registrationButton = document.querySelector("#registration");
+registrationButton.href = `/registration?appId=${appId}`;
 // passkey login
 // 1. assertion challenge request (get /assertion)
-(() => {
+passKeyLoginButton.addEventListener("click", async (event) => {
+    event.preventDefault();
     fetch("/passkey/assertion")
-        .then(response => {
-            if (!response.ok) throw new Error("서버 오류");
-            return response.json();
-        })
-        .then(data => {
-            const challenge = data.data.publicKeyCredentialRequestOptions.challenge;
-            loginPasskey(challenge);
-        })
-        .catch(err => {
-            console.error("API 실패:", err);
-        });
-})();
+          .then(response => {
+              if (!response.ok) throw new Error("서버 오류");
+              return response.json();
+          })
+          .then(data => {
+              const challenge = data.data.publicKeyCredentialRequestOptions.challenge;
+              loginPasskey(challenge);
+          })
+          .catch(err => {
+              console.error("API 실패:", err);
+          });
+});
 
 // 2. passkey 추출
 function toBase64Url(base64) {
@@ -44,39 +48,46 @@ async function loginPasskey(challenge) {
 
         const credential = await navigator.credentials.get({
               publicKey: publicKey
-        });
-
-        const assertionResponse = {
-            id: credential.id,
-            rawId: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.rawId)))),
-            type: credential.type,
-            response: {
-                authenticatorData: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.authenticatorData)))),
-                clientDataJSON: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.clientDataJSON)))),
-                signature: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.signature)))),
-                userHandle: credential.response.userHandle ? toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.userHandle)))) : null
-            },
-            clientExtensionResults: credential.getClientExtensionResults()
-        };
-
-        const passkeyResponse = await fetch(`/passkey/assertion`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(assertionResponse)
-        });
-
-        if (passkeyResponse.ok) {
-            console.log(passkeyResponse)
+            });
+        console.log(credential);
+        if (credential) {
+            await loginRequest(credential);
         } else {
-            alert("Login failed.");
+            window.location.href = `/registration?appId=${appId}`;
         }
     } catch (error) {
         console.error("패스키 로그인이 실패했습니다.", error);
     }
 }
 
+async function loginRequest(credential) {
+     const assertionResponse = {
+                id: credential.id,
+                rawId: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.rawId)))),
+                type: credential.type,
+                response: {
+                    authenticatorData: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.authenticatorData)))),
+                    clientDataJSON: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.clientDataJSON)))),
+                    signature: toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.signature)))),
+                    userHandle: credential.response.userHandle ? toBase64Url(btoa(String.fromCharCode(...new Uint8Array(credential.response.userHandle)))) : null
+                },
+                clientExtensionResults: credential.getClientExtensionResults()
+            };
+
+    const passkeyResponse = await fetch(`/passkey/assertion`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(assertionResponse)
+    });
+
+    if (passkeyResponse.ok) {
+        console.log(passkeyResponse)
+    } else {
+        alert("Login failed.");
+    }
+}
 // 4. TODO: post /assertion이 access_token을 반환하도록 변경
 
 
